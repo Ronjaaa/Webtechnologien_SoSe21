cac5ea1e9859613f2f84af27228340486dd26e4c
Unknown <ronja.slabon@gmail.com>
2021-06-01 10:09:12 +0200

    up

---
+++ Responsive Webdesign/app.js
@@ -0,0 +1,234 @@
+//Gold Animation
+const flightPath = {
+    curviness: 0,
+    autoRotate: false,
+    values: [
+        {x: 0, y: -10},
+        {x: 0, y: -30},
+        {x: 0, y: -50},
+        {x: 0, y: -70},
+        {x: 0, y: -90},
+        {x: 0, y: -110}
+    ]
+}
+
+//Timeline
+const tween = new TimelineMax();
+
+tween.add(
+    TweenMax.to(".gold-lipstick", 1, {
+        bezier: flightPath,
+        ease: Power1.easeInOut
+    })
+);
+
+//const blueanmimation = document.querySelector(".blueanmimation");
+//const video = blueanmimation.querySelector("video");
+//const text = blueanmimation.querySelector("h1");
+
+//End Section
+//const section = document.querySelector("section");
+//const end = section.querySelector("h1");
+
+
+
+//ScrollMagic
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: ".goldanimation",
+    duration: 1000,
+    triggerHook: 0
+})
+.setTween(tween)
+//.addIndicators()
+.setPin(".goldanimation")
+.addTo(controller);
+
+//Text Animation
+const goldanimation = document.querySelector(".goldanimation");
+const text = goldanimation.querySelector("h1");
+const textAnim = TweenMax.fromTo(text, 1, {opacity: 0}, {opacity: 1});
+
+let scene2 = new ScrollMagic.Scene({
+    duration: 500,
+    triggerElement: ".goldanimation",
+    triggerHook: 0
+})
+.setTween(textAnim)
+.addTo(controller);
+
+//Intro
+const flightPathIntro = {
+    curviness: 0,
+    autoRotate: false,
+    values: [
+        {x: 0, y: -10},
+        {x: 0, y: -30},
+        {x: 0, y: -50},
+        {x: 0, y: -70},
+        {x: 0, y: -90},
+        {x: 0, y: -110}
+    ]
+}
+
+//Timeline
+const tweenIntro = new TimelineMax();
+
+tweenIntro.add(
+    TweenMax.to(".gold", 1, {
+        bezier: flightPathIntro,
+        ease: Power1.easeInOut
+    })
+);
+
+const scene4 = new ScrollMagic.Scene({
+    triggerElement: ".intro",
+    duration: 1000,
+    triggerHook: 0
+})
+.setTween(tweenIntro)
+//.addIndicators()
+.setPin(".intro")
+.addTo(controller);
+
+//Text Animation Intro
+const introAnimation = document.querySelector(".intro");
+const textIntro = introAnimation.querySelector("h1");
+const textAnimIntro = TweenMax.fromTo(textIntro, 1, {opacity: 1}, {opacity: 0});
+
+let scene3 = new ScrollMagic.Scene({
+    duration: 1000,
+    triggerElement: ".intro",
+    triggerHook: 0
+})
+.setTween(textAnimIntro)
+.addTo(controller);
+
+//Green Animation
+const flightPathGreen = {
+    curviness: 0,
+    autoRotate: false,
+    values: [
+        {x: 0, y: -10},
+        {x: 0, y: -30},
+        {x: 0, y: -50},
+        {x: 0, y: -70},
+        {x: 0, y: -90},
+        {x: 0, y: -110}
+    ]
+}
+
+//Timeline
+const tweenGreen = new TimelineMax();
+
+tweenGreen.add(
+    TweenMax.to(".green-lipstick", 1, {
+        bezier: flightPathGreen,
+        ease: Power1.easeInOut
+    })
+);
+
+const scene5 = new ScrollMagic.Scene({
+    triggerElement: ".greenanimation",
+    duration: 1000,
+    triggerHook: 0
+})
+.setTween(tweenGreen)
+//.addIndicators()
+.setPin(".greenanimation")
+.addTo(controller);
+
+//Text Animation
+const greenanimation = document.querySelector(".greenanimation");
+const textGreen = greenanimation.querySelector("h1");
+const textAnimGreen = TweenMax.fromTo(textGreen, 1, {opacity: 0}, {opacity: 1});
+
+let scene6 = new ScrollMagic.Scene({
+    duration: 500,
+    triggerElement: ".greenanimation",
+    triggerHook: 0
+})
+.setTween(textAnimGreen)
+.addTo(controller);
+
+//Red Animation
+const flightPathRed = {
+    curviness: 0,
+    autoRotate: false,
+    values: [
+        {x: 0, y: -10}, {x: 0, y: -30}, {x: 0, y: -50}, {x: 0, y: -70}, {x: 0, y: -90}, {x: 0, y: -110}, {x: 0, y: -130}, 
+        {x: 0, y: -150}, {x: 0, y: -170}, {x: 0, y: -190}, {x: 0, y: -200}, {x: 0, y: -220}, {x: 0, y: -240}
+    ]
+}
+//Timeline
+const tweenRed = new TimelineMax();
+
+tweenRed.add(
+    TweenMax.to(".red-lipstick", 1, {
+        bezier: flightPathRed,
+        ease: Power1.easeInOut
+    })
+);
+
+const scene7 = new ScrollMagic.Scene({
+    triggerElement: ".redanimation",
+    duration: 1000,
+    triggerHook: 0
+})
+.setTween(tweenRed)
+//.addIndicators()
+.setPin(".redanimation")
+.addTo(controller);
+
+//Text Animation
+const redanimation = document.querySelector(".redanimation");
+const textRed = redanimation.querySelector("h1");
+const textAnimRed = TweenMax.fromTo(textRed, 1, {opacity: 0}, {opacity: 1});
+
+let scene8 = new ScrollMagic.Scene({
+    duration: 500,
+    triggerElement: ".redanimation",
+    triggerHook: 0
+})
+.setTween(textAnimRed)
+.addTo(controller);
+
+
+//Scenes
+//let scene = new ScrollMagic.Scene({
+//    duration: 9000, //9sec
+//    trigerElement: blueanmimation,
+//    triggerHook: 0
+//}) 
+//.addIndicators()
+//.setPin(blueanmimation) //pint da video fest bis fertig
+//.addTo(controller);
+
+//Text Animation
+//const textAnim = TweenMax.fromTo(text, 3, {opacity: 0}, {opacity: 1});
+
+//let scene2 = new ScrollMagic.Scene({
+//    duration: 3000,
+//    triggerElement: blueanmimation,
+//    triggerHook: 0
+//})
+//.setTween(textAnim)
+//.addTo(controller);
+
+//Video Animation
+//let accelamount = 0.1;
+//let scrollpos = 0;
+//let delay = 0;
+
+//scene.on("update", e => {
+//    scrollpos = e.scrollPos / 1000;
+ //   console.log(e);
+//});
+
+//setInterval(() => {
+//    delay += (scrollpos - delay) * accelamount;
+//    console.log(scrollpos, delay);
+//    video.currentTime = delay;
+//}, 33.3); 
+
