40ca9ac10b4925b4e9c086a153b1b21ba19947a2
Ronja <64131821+Ronjaaa@users.noreply.github.com>
2021-06-15 15:20:30 +0200

    test

---
+++ Responsive Webdesign/script.js
@@ -0,0 +1,25 @@
+"use strict";
+/*Eventlistener Burger menu*/
+const menuBtn = document.querySelector(".icon");
+let menuOpen = flase;
+menuBtn.addEventListener("click", () => {
+  if(!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen= true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen =false;
+  }
+});
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
